cf85d62d7ea119ed7c008f5a732407cbd35c100e
KNSN92 <kekenshin92@gmail.com>
2026-02-18 14:23:19 +0900

    fix: baseUrlはgithubのリポジトリ名と一緒じゃ無きゃダメなのね

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/KasuReact/",
+  base: "/Kasu-React-Generator/",
   plugins: [
     react({
       babel: {
